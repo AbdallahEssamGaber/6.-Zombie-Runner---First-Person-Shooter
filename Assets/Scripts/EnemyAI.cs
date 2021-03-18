813db5a4a1df78dd13f932bb8d3725efc2b5b651
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2021-03-18 15:16:05 +0200

    7. Enemy AI - Chase Range
    we made chase range if the distance less than it then go get him

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
+    NavMeshAgent navMeshAgent;
+    float rangeToEnemy = Mathf.Infinity;    //to not causing problems in the future "PROGRAMMING"
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        rangeToEnemy = Vector3.Distance(target.position, transform.position);
+        if(rangeToEnemy <= chaseRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+
+    }
+}
