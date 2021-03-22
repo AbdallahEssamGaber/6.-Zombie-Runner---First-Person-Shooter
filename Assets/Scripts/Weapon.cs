b94175cb192f7ab0fec0cb69a35ac0bc3b2fbbb1
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2021-03-22 15:06:29 +0200

    12. Introduction To Raycasting + very tiny thing for fun!
    So this video basically was talking abt Raycasting so its just smth realted to physics can make shooting thing or whatever just put the starting point and where it will goes  or in what direction..., put a variable have the end point to save it to use it other time w the keyword out....... and finally range
    
    --> And Also I add two things or two different things....first one is dont print if u hit anything out of the game plane at least like the sky dosent count....And after that I made smth dont make a hit or shoot if the things u shoot on is not Enemy

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    [SerializeField] Camera cameraObject;
+    [SerializeField] float range = 0f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Physics.Raycast(cameraObject.transform.position, cameraObject.transform.forward, out RaycastHit hit, range);
+        //if (hit.transform != null)
+        //{
+
+        //    print(hit.transform.name);
+
+        //}
+        if (hit.transform.name == "Enemy")
+        {
+
+            print(hit.transform.name);
+
+        }
+    }
+}
