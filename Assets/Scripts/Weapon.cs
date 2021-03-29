34dc6a72a346e1c1d6831200707baade8a3cea51
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2021-03-29 12:37:10 +0200

    14. Implement A Muzzle Flash
    So in this video we put Particles when we shoot(Muzzle Falsh)

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,6 +9,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera cameraObject;
     [SerializeField] float range = 0f;
     [SerializeField] int damage = 1;
+    [SerializeField] ParticleSystem muzzleFlash;
 
     void Update()
     {
@@ -20,8 +21,15 @@ public class Weapon : MonoBehaviour
 
     void Shoot()
     {
-        if(Physics.Raycast(cameraObject.transform.position, cameraObject.transform.forward, out RaycastHit hit, range))
+        muzzleFlash.Play();
+        RaycastThing();
+    }
+
+    void RaycastThing()
+    {
+        if (Physics.Raycast(cameraObject.transform.position, cameraObject.transform.forward, out RaycastHit hit, range))
         {
+
             Debug.Log(hit.transform.name);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
             if (target == null) return;     //cuz the error if u hit smth dont have EnemyHealth
