1b543abc51a58e91e6e880d89ee5ca2c8264324d
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2021-04-08 00:38:16 +0200

    21. Use Animation Events
    Yo so what abt if u want to make animation event like in this min of this animation do smth in code how to do it that whats video talking abt

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float health = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    public void AttackHitEvent()
+    {
+        if (target == null) return;
+        print("bang bang");
+    }
+}
