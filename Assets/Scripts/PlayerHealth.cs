86362ca63db5053052530e0c80e345b00cee8c3f
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2021-04-08 01:40:50 +0200

    22. Create Player Health Class
    make player health system

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] EnemyAttack enemyAttack;
+    public float hitPoints = 5f;
+
+   public void TakeDamage(float deamage)
+    {
+        hitPoints -= deamage;
+        if (hitPoints <= 0)
+        {
+            Debug.Log("dead bitch!");
+        }
+    }
+
+   
+
+ 
+}
