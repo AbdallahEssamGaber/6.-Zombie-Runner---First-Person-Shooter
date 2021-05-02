c1d108aa03f7413b82bfa07ec51c67c35b2c8f1f
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2021-05-02 11:37:26 +0200

    24. Game Over User Interface
    Making buttons and text prototype for   -you dead

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void ReloadGame()
+    {
+        SceneManager.LoadScene(0);
+    } 
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
