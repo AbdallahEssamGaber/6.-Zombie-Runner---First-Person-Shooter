eff2d92eca9354b0befeb333661accaf5c12fc00
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2021-05-12 06:31:18 +0200

    36. Player Input To Select Weapon
    switching weapons by scroll wheel and numbers

--- Assets/Scripts/WeaponSwitcher.cs
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -10,7 +10,65 @@ public class WeaponSwitcher : MonoBehaviour
 
     void Start()
     {
-        SetWeaponActive();
+        SetWeaponActive();  //thats to turn just one weapon in the first without clicking anything...
+    }
+
+    void Update()
+    {
+        int previousWeapon = currentWeapon;
+
+        processKeyInput();
+        processScrollWheel();
+
+        if (previousWeapon != currentWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+
+ 
+    void processKeyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentWeapon = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentWeapon = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentWeapon = 2;
+        }
+    }
+
+    void processScrollWheel()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (currentWeapon >= transform.childCount - 1)
+            {
+                currentWeapon = 0;
+            }
+            else
+            {
+                currentWeapon++;
+            }
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (currentWeapon <= 0)
+            {
+                currentWeapon = transform.childCount - 1;
+            }
+            else
+            {
+                currentWeapon--;
+            }
+        }
+
+
     }
 
     void SetWeaponActive()
