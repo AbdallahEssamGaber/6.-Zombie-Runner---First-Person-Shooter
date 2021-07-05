1971a9e52b793b672d3cb541017b98ebf57fd2bf
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2021-07-05 11:58:27 +0200

    Enemy(headshot - effects on hit)

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -10,7 +10,8 @@ public class Weapon : MonoBehaviour
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 30f;
     [SerializeField] ParticleSystem muzzleFlash;
-    [SerializeField] GameObject hitEffect;
+    [SerializeField] GameObject hitEffectRest;
+    [SerializeField] GameObject hitEffectBody;
     [SerializeField] Ammo ammoSlot;
     [SerializeField] AmmoType ammoType;
     [SerializeField] float timeBetweenShots = 0.5f;
@@ -20,7 +21,7 @@ public class Weapon : MonoBehaviour
 
 
     bool canShoot = true;
-
+    bool where;
   
 
 
@@ -94,16 +95,22 @@ public class Weapon : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
-            CreateHitImpact(hit);
+
             if (hit.transform.tag == "head")
             {
+                CreateHitImpactBody(hit);
                 EnemyHealth target = hit.transform.GetComponentInParent<EnemyHealth>();
-                target.TakeDamage(20f);
+                target.TakeDamage(damage * 2);
             }
             else if (hit.transform.tag == "enemy")
             {
+                CreateHitImpactBody(hit);
                 EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
                 target.TakeDamage(damage);
+            }
+            else 
+            {
+                CreateHitImpactRest(hit);
             }
             
             
@@ -124,9 +131,14 @@ public class Weapon : MonoBehaviour
 
   
 
-    void CreateHitImpact(RaycastHit hit)
+    void CreateHitImpactRest(RaycastHit hit)
     {
-        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        GameObject impact = Instantiate(hitEffectRest, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, .1f);
+    } 
+    void CreateHitImpactBody(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffectBody, hit.point, Quaternion.LookRotation(hit.normal));
         Destroy(impact, .1f);
     }
 
